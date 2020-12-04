a4bec04789a5d76fd2d5af3880ad8a049db46225
laur80 <laur80raul@gmail.com>
2020-12-04 21:26:13 +0200

    Updates

---
+++ precache-manifest.9a97c0b7a147355c507816ce66871d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97eda24e4cd1d8088b17",
+    "url": "/dad-jokes/static/css/main.ed80b2c4.chunk.css"
+  },
+  {
+    "revision": "97eda24e4cd1d8088b17",
+    "url": "/dad-jokes/static/js/main.97eda24e.chunk.js"
+  },
+  {
+    "revision": "edbd401cd1b73c73842a",
+    "url": "/dad-jokes/static/js/1.edbd401c.chunk.js"
+  },
+  {
+    "revision": "f2047619ceef94a5772e",
+    "url": "/dad-jokes/static/js/runtime~main.f2047619.js"
+  },
+  {
+    "revision": "2046501a19460817cc3aeb53f576c61f",
+    "url": "/dad-jokes/index.html"
+  }
+];
